b87f93fbeeecc56faac7ecc8483e321cd8230093
Eusuf Uddin <eusufuddin1234@gmail.com>
2020-07-10 22:13:29 +0600

    first commit

---
+++ app/src/main/java/com/example/coordinator_layoutdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.coordinator_layoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mShowSnackbarButton;
+    private CoordinatorLayout mCoordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+        mShowSnackbarButton = (Button) findViewById(R.id.showSnackbarButton);
+        mShowSnackbarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(mCoordinatorLayout,
+                        "This is a simple Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("CLOSE", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Custom action
+                            }
+                        }).show();
+            }
+        });
+    }
+}
